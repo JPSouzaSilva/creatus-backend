edfff4f6bfe26b808cb0ec9e1e59b06c935fc456
João Silva <jpsouzadasilva@gmail.com>
2024-07-15 01:39:12 -0300

    att xanflis

--- src/http/controllers/user/report-generation.ts
+++ src/http/controllers/user/report-generation.ts
@@ -1,8 +1,10 @@
 import { makeReportGenerationFactory } from '@/factories/make-report-generation'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import fs from 'fs'
+import path from 'path'
 
 export async function reportGeneration(
-  request: FastifyRequest,
+  _request: FastifyRequest,
   reply: FastifyReply,
 ) {
   try {
@@ -12,6 +14,14 @@ export async function reportGeneration(
 
     reply.header('Content-Type', 'text/csv')
     reply.header('Content-Disposition', 'attachment; filename=report.csv')
+
+    const filePath = path.join('src', 'reports', 'report.csv')
+
+    fs.writeFile(filePath, csv, (err) => {
+      if (err) {
+        reply.status(500).send({ message: 'Erro ao salvar arquivo CSV' })
+      }
+    })
 
     reply.status(201).send(csv)
   } catch (error) {
